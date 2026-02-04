76820dd69a366ba24504faa36dad176a55ea27c3
Igor <Sketch_90@mail.ru>
2026-02-04 22:14:38 +0300

    fix packages

--- src/main/java/ru/pozdeev/whispererservice/WhispererApplication.java
+++ src/main/java/ru/pozdeev/whispererservice/WhispererApplication.java
@@ -1,8 +1,10 @@
-package ru.pozdeevwhispererservice;
+package ru.pozdeev.whispererservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class WhispererApplication {
     public static void main(String[] args) {
